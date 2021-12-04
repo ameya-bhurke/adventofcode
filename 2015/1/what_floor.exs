5ce7a68e19c21bee27c3991586cf5137ed2da8d6
Ameya Bhurke <ameyabhurke@gmail.com>
2021-12-04 18:30:09 +1100

    rename count to total

--- 2015/1/what_floor.exs
+++ 2015/1/what_floor.exs
@@ -3,8 +3,8 @@ defmodule Floor do
   def score(")"), do: -1
   def score(_), do: 0
 
-  def process([], count), do: count
-  def process([head | tail], count), do: process(tail, score(head) + count)
+  def process([], total), do: total
+  def process([head | tail], total), do: process(tail, score(head) + total)
 end
 
 IO.puts(Floor.process(String.graphemes(File.read!("input.txt")), 0))
