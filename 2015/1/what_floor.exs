27bf1853a7fbe0bb83bf2c12ffe496d7ea2c8448
Ameya Bhurke <ameyabhurke@gmail.com>
2021-12-04 18:43:40 +1100

    part 2

--- 2015/1/what_floor.exs
+++ 2015/1/what_floor.exs
@@ -5,6 +5,12 @@ defmodule Floor do
 
   def process([], total), do: total
   def process([head | tail], total), do: process(tail, score(head) + total)
+
+  def process_2([_ | _], {total, count}) when total == -1, do: count
+
+  def process_2([head | tail], {total, count}), do: process_2(tail, {score(head) + total, count + 1})
+
 end
 
 IO.puts(Floor.process(String.graphemes(File.read!("input.txt")), 0))
+IO.puts(Floor.process_2(String.graphemes(File.read!("input.txt")), {0, 0}))
