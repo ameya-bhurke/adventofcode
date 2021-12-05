483116a08d4f7bb6d34f386a21a9f28ea8bdabde
Ameya Bhurke <ameyabhurke@gmail.com>
2021-12-05 13:35:05 +1100

    added functions for calculating ribbon length

--- 2015/2/wrapping_paper.exs
+++ 2015/2/wrapping_paper.exs
@@ -7,7 +7,28 @@ defmodule Area do
     |> Enum.min
   end
 
-  def area(l, w, h) do
+  def least2(areas) do
+    areas
+    |> Tuple.to_list
+    |> Enum.sort
+    |> Enum.take(2)
+  end
+
+  def ribbon(areas) do
+    areas
+    |> least2
+    |> Enum.map(fn x -> x * 2 end)
+    |> Enum.sum
+  end
+
+  def wrapping(areas) do 
+    ribbon(areas)
+    +
+    areas |>
+    Enum.product
+  end
+
+  def paper(l, w, h) do
     min = sides(l, w, h)
     |> least
     
@@ -22,7 +43,7 @@ end
 File.stream!("input")
 |> Stream.map( &String.replace(&1, "\n", "") )
 |> Stream.map( &String.split(&1, "x") )
-|> Stream.map( &Area.area(String.to_integer(Enum.at(&1,0)), String.to_integer(Enum.at(&1, 1)), String.to_integer(Enum.at(&1, 2)) ) )
+|> Stream.map( &Area.paper(String.to_integer(Enum.at(&1,0)), String.to_integer(Enum.at(&1, 1)), String.to_integer(Enum.at(&1, 2)) ) )
 |> Enum.to_list()
 |> Enum.sum
 |> IO.puts
